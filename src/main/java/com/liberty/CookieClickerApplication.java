cb91ce5fb8f5b7809f97dd17c224573d8fa38995
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-05-11 17:24:44 +0300

    Initial_commit

---
+++ src/main/java/com/liberty/CookieClickerApplication.java
@@ -0,0 +1,12 @@
+package com.liberty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieClickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieClickerApplication.class, args);
+	}
+}
